e56ff3286d07f0f14a5ed7816b44bf252a394919
Leela Narasimha <leelanrp@qualminds.com>
2020-03-16 20:09:50 +0530

    Added products code

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,9 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	console.log(req.User);
-	req.user
-		.createProduct({
-			title,
-			imageUrl,
-			price,
-			description
-		})
+	const product = new Product(title, description, imageUrl, price);
+	product
+		.save()
 		.then((response) => {
 			res.redirect('/');
 		})
--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,16 @@ class Product {
 
 	save() {
 		const db = getDb();
-		db.collection('products').insertOne(this);
+		return db
+			.collection('products')
+			.insertOne(this)
+			.then((result) => {
+				console.log(result);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}
 }
+
+module.exports = Product;
